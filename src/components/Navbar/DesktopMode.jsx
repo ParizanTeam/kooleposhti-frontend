7bb2d268027ed9addc1add2fabf3343b9b01a124
Bayan <bayany1379@gmail.com>
2021-10-21 16:38:08 +0330

    [feature]: Add Navbar

---
+++ src/components/Navbar/DesktopMode.jsx
@@ -0,0 +1,59 @@
+// IMPORTING APIS
+import React from 'react';
+import './style.scss';
+import logo from "../../assets/logo.png";
+
+
+import { AppBar, Toolbar, Typography, Button, useScrollTrigger, Slide } from '@mui/material';
+
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+
+// IMPORTING ICONS
+import SchoolIcon from '@mui/icons-material/School';
+import HelpIcon from '@mui/icons-material/Help';
+
+const DesktopNavbar = props => {
+  return (
+    <div id="desktop">
+        <AppBar color="transparent">
+          <Toolbar>
+            <Typography variant="h5" component="p" color="textSecondary">
+              کوله‌پشتی
+            </Typography>
+            {
+              <div style={{ marginRight: '2rem' }}>
+                <Button variant="text" component={Link} to="/singup" color="success">
+                  <SchoolIcon />
+                  تدریس کن
+                </Button>
+                <Button variant="text" component={Link} to="/Help" color="success">
+                  <HelpIcon />
+                  راهنما
+                </Button>
+              </div>
+            }
+            {
+              <div style={{ marginRight: 'auto', marginLeft: '10%'}}>
+                <Button
+                  variant="contained"
+                  component={Link}
+                  to="/singup"
+                  color="success"
+                  style={{ marginLeft: '10px' }}
+                >
+                  ثبت نام
+                </Button>
+                <Button variant="contained" component={Link} to="/login" color="success">
+                  ورود
+                </Button>
+              </div>
+            }
+          </Toolbar>
+        </AppBar>
+    </div>
+  );
+};
+
+
+
+export default DesktopNavbar;
