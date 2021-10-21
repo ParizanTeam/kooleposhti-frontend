a2f13ad7f5324fbf0f142bc7b4a7a6312d283c6b
morteza shahrabi farahani <morteza.shahrabii@gmail.com>
2021-10-21 13:36:51 +0330

    [feature]adding captcha

--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import Recaptcha from 'react-recaptcha';
+
 import './style.scss';
 
 const LoginPage = () => {
-  return <h1>صفحه ورود</h1>;
+  return (
+    <div>
+      <Recaptcha
+        sitekey="6LfI6N4cAAAAAM5s9zVuo5MJiUYbHYO9Du9cgJSU"
+        render="expilcit"
+        onloadCallback={() => console.log('loaded')}
+      />
+
+      <h1>صفحه ورود</h1>
+    </div>
+  );
 };
 
 export default LoginPage;
