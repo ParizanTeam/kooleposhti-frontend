89d9b7566a6fbea3697391cbcccdfa519e7d734a
Hani <hani.j.2000@gmail.com>
2021-10-26 23:28:45 +0330

    [feature]: Add NotFoundPage

--- src/components/NotFoundPage/index.jsx
+++ src/components/NotFoundPage/index.jsx
@@ -3,9 +3,16 @@ import { Link } from 'react-router-dom';
 import './style.scss';
 
 const NotFoundPage = () => (
-  <div>
-    <h1>۴۰۴ - صفحه مورد نظر یافت نشد</h1>
-    <Link to="/">بازگشت به خانه</Link>
+  <div className = 'back'>
+    <div className = 'NotF'>
+    <img src="https://8pic.ir/uploads/404-boo-pic.jpg" alt="404" className = 'NotF__Media'/>
+    <div className = 'NotF__Content'>
+    <h1>اوه! هیچی اینجا نیست...</h1></div>
+    </div>
+    <div className = 'ComebackHome'>
+    <img src="https://8pic.ir/uploads/PngItem-5957531.png" alt="404" className = 'ComebackHome__Media'/>
+    <Link to="/">برگردیم خونه؟</Link>
+    </div>
   </div>
 );
 
