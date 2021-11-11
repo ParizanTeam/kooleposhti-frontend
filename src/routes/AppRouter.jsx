5f362453653f46ff0a87a9adacef0ad336093014
Hanieh Jafari <hani.j.2000@gmail.com>
2021-11-11 18:56:16 +0330

    [feat]: add ClassCalendar

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -9,9 +9,9 @@ import ResetPasswordPage from '../components/ResetPasswordPage';
 import TeacherDashboard from '../components/TeacherDashboard';
 import ClassSchedule from '../components/ClassSchedule';
 import BookMarkedClasses from '../components/BookMarkedClasses';
-import SearchClasses from '../components/SearchClasses';
 import { Redirect } from 'react-router';
 import StudentProfile from '../components/StudentProfile';
+import ClassCalendar from '../components/ClassCalendar';
 
 const AppRouter = () => {
   return (
@@ -55,8 +55,8 @@ const AppRouter = () => {
           <Route path="/dashboard/student/" exact>
             <Redirect to="/dashboard/student/Profile" />
           </Route>
-          <Route path="/dashboard/student/Search">
-            <SearchClasses />
+          <Route path="/dashboard/student/Calendar">
+            <ClassCalendar />
           </Route>  
           <Route path="/dashboard/student/Profile">
             <StudentProfile />
