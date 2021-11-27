af802df60628dc262d45df388f3cee13400eedeb
Bayan <bayany1379@gmail.com>
2021-11-28 03:18:59 +0330

    [feat]: connect myclasses to back

--- src/components/ClassSchedule/index.jsx
+++ src/components/ClassSchedule/index.jsx
@@ -1,6 +1,6 @@
 //import { useMobile } from '../../utils/detectSource';
 import MyClasses from '../MyClasses';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MyClassesFooter from '../MyClasses/MyClassesFooter';
 import { useMobile } from '../../utils/detectSource';
 import { coursesData } from '../CourseSlider/coursesData';
@@ -11,210 +11,116 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import { Select, MenuItem } from '@mui/material';
+import { Select, MenuItem, Link } from '@mui/material';
+import axios from '../../utils/axiosConfig';
+import { baseUrl } from '../../utils/constants';
+import { changeDateFormat, dateDiff } from '../../utils/helpers';
+import ReactLoading from 'react-loading';
 
 import './style.scss';
+import CourseSlider from '../CourseSlider';
 
-const ClassCarda = () => {
-  return (
-    <Card sx={{ maxWidth: 350, boxShadow: '8px 8px 6px #888888', m: '20px', textAlign: 'center' }}>
-      <CardMedia
-        component="img"
-        height="180"
-        image='https://process.filepicker.io/APHE465sSbqvbOIStdwTyz/rotate=deg:exif/resize=fit:crop,height:283,width:472/output=quality:80,compress:true,strip:true,format:jpg/cache=expiry:max/https://cdn.filestackcontent.com/TcAWV8mZShCS36mN0Uei'
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        آشنایی با معماری
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          مدرس: افشین زنگنه
-          <br />
-          روز های: دوشنبه سه شنبه ها
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button sx={{ margin: 'auto' }}>ورود به کلاس</Button>
-      </CardActions>
-    </Card>
-  );
-};
-
-
-const ClassCardd = () => {
-  return (
-    <Card sx={{ maxWidth: 350, boxShadow: '8px 8px 6px #888888', m: '20px', textAlign: 'center' }}>
-      <CardMedia
-        component="img"
-        height="180"
-        image="https://process.filepicker.io/APHE465sSbqvbOIStdwTyz/rotate=deg:exif/resize=fit:crop,height:283,width:472/output=quality:80,compress:true,strip:true,format:jpg/cache=expiry:max/https://cdn.filestackcontent.com/dAqePyb4R5CQN1FfEHcZ"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        تندخوانی 
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          مدرس: غزل بخشنده 
-          <br />
-          روز های: دوشنبه سه شنبه ها
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button sx={{ margin: 'auto' }}>ورود به کلاس</Button>
-      </CardActions>
-    </Card>
-  );
-};
-
-const ClassCarde = () => {
-  return (
-    <Card sx={{ maxWidth: 350, boxShadow: '8px 8px 6px #888888', m: '20px', textAlign: 'center' }}>
-      <CardMedia
-        component="img"
-        height="180"
-        image="https://process.filepicker.io/APHE465sSbqvbOIStdwTyz/rotate=deg:exif/resize=fit:crop,height:283,width:472/output=quality:80,compress:true,strip:true,format:jpg/cache=expiry:max/https://cdn.filestackcontent.com/KeX9jdVyT8CHvIW3xX46"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        آموزش گیتار برای کودکان
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          مدرس: دانیال بازمانده 
-          <br />
-          روز های: دوشنبه سه شنبه ها
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button sx={{ margin: 'auto' }}>ورود به کلاس</Button>
-      </CardActions>
-    </Card>
-  );
-};
-
-const ClassCardf= () => {
-  return (
-    <Card sx={{ maxWidth: 350, boxShadow: '8px 8px 6px #888888', m: '20px', textAlign: 'center' }}>
-      <CardMedia
-        component="img"
-        height="180"
-        image="https://process.filepicker.io/APHE465sSbqvbOIStdwTyz/rotate=deg:exif/resize=fit:crop,height:283,width:472/output=quality:80,compress:true,strip:true,format:jpg/cache=expiry:max/https://cdn.filestackcontent.com/nNTBArS9S2i2PMEEWJOf"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        نویسندگی خلاقانه' 
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          مدرس: بیان دیوانی آذر 
-          <br />
-          روز های: دوشنبه سه شنبه ها
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button sx={{ margin: 'auto' }}>ورود به کلاس</Button>
-      </CardActions>
-    </Card>
-  );
-};
-
-
-const ClassCardb = () => {
-  return (
-    <Card sx={{ maxWidth: 350, boxShadow: '8px 8px 6px #888888', m: '20px', textAlign: 'center' }}>
-      <CardMedia
-        component="img"
-        height="180"
-        image="https://process.filepicker.io/APHE465sSbqvbOIStdwTyz/rotate=deg:exif/resize=fit:crop,height:283,width:472/output=quality:80,compress:true,strip:true,format:jpg/cache=expiry:max/https://cdn.filestackcontent.com/bCpGofJTliqN6MltJhvQ"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          آموزش شطرنج
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          مدرس: حانیه جعفری
-          <br />
-          روز های: دوشنبه سه شنبه ها
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button sx={{ margin: 'auto' }}>ورود به کلاس</Button>
-      </CardActions>
-    </Card>
-  );
-};
-
-const ClassCardc = () => {
-  return (
-    <Card sx={{ maxWidth: 350, boxShadow: '8px 8px 6px #888888', m: '20px', textAlign: 'center' }}>
-      <CardMedia
-        component="img"
-        height="180"
-        image="https://process.filepicker.io/APHE465sSbqvbOIStdwTyz/rotate=deg:exif/resize=fit:crop,height:283,width:472/output=quality:80,compress:true,strip:true,format:jpg/cache=expiry:max/https://cdn.filestackcontent.com/7MNa1W9jSyKYoKW5MMUl"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        سخنرانی در جمع
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          مدرس: علی صداقی
-          <br />
-          روز های: دوشنبه سه شنبه ها
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button sx={{ margin: 'auto' }}>ورود به کلاس</Button>
-      </CardActions>
-    </Card>
-  );
-};
 const ClassSchedule = () => {
-  const [showClass, setShowClass] = React.useState('');
+  const [loading, setLoading] = React.useState(true);
+  const [classStatus, setClassStatus] = React.useState('active');
 
+  const [classData, setClassData] = useState([]);
+  const DisplayClass = date => {
+    const isPast = dateDiff(date) >= -1 ? true : false;
+    if (classStatus === 'active') return isPast;
+    else if (classStatus === 'past') return !isPast;
+    else return true;
+  };
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios
+        .get(`${baseUrl}/accounts/students/classes/`)
+        .then(response => {
+          console.log('classData ', response.data.courses);
+          setClassData(response.data.courses);
+          setLoading(false);
+        })
+        .catch(err => {
+          console.log('error: ', err);
+        });
+    }
+    fetchData();
+  }, []);
   const handleChange = event => {
-    setShowClass(event.target.value);
+    setClassStatus(event.target.value);
   };
   return (
     <div>
       <MyClasses />
-      <img src='https://8pic.ir/uploads/1307925801537355428-128.png' alt='cs' className='csImg'/>
+      <img src="https://8pic.ir/uploads/1307925801537355428-128.png" alt="cs" className="csImg" />
       <br />
       <div className="afterMyC-c">
-        <div style={{marginRight:"100px"}}> 
-          <Select value={showClass} onChange={handleChange} displayEmpty  MenuProps={{ disableScrollLock: true }}>
-            <MenuItem value="">کلاس های فعال</MenuItem>
-            <MenuItem value="pastClasses">کلاس های گذشته</MenuItem>
+        <div style={{ marginRight: '100px' }}>
+          <Select value={classStatus} onChange={handleChange} displayEmpty MenuProps={{ disableScrollLock: true }}>
+            <MenuItem value="active">کلاس های فعال</MenuItem>
+            <MenuItem value="past">کلاس های گذشته</MenuItem>
+            <MenuItem value="all">همه کلاس ها</MenuItem>
           </Select>
         </div>
 
-        <div style={{ display: 'flex', justifyContent: 'center',textAlign:'center',marginBottom: "30px" }}>
-          <Grid justifyContent="center" alignItems="center" container rowSpacing={4} md={8} xs={10}>
-            <Grid item md={4} sm={6} xs={12}>
-              <ClassCarda />
-            </Grid>
-            <Grid item md={4} sm={6} xs={12}>
-              <ClassCardb />
-            </Grid>
-            <Grid item md={4} sm={6} xs={12}>
-              <ClassCardc />
-            </Grid>
-            <Grid item md={4} sm={6} xs={12}>
-              <ClassCardd />
-            </Grid>
-            <Grid item md={4} sm={6} xs={12}>
-              <ClassCarde />
-            </Grid>
-            <Grid item md={4} sm={6} xs={12}>
-              <ClassCardf />
-            </Grid>
-          </Grid>
+        <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center', marginBottom: '30px' }}>
+          {loading ? (
+            <div style={{ padding: '100px',display: 'flex' }}>
+              <ReactLoading type="spinningBubbles" color="orangered" height={100} width={100} />{' '}
+            </div>
+          ) : (
+            <>
+              {classData.length === 0 && (
+                <div>
+                  <Typography  variant="h6" component="div" style={{ padding: '50px 0', color: 'grey'}} >
+                    کلاسی نداری چطوره یه نگاهی به کلاسای اینجا بندازی؟
+                  </Typography>
+                  <CourseSlider/>
+                </div>
+              )}
+              <Grid style={{display: 'flex'}} justifyContent="center" alignItems="center" container rowSpacing={4} md={8} xs={10}>
+                {classData.map(classInfo => (
+                  <>
+                    {DisplayClass(classInfo.end_date) && (
+                      <Grid item md={4} sm={6} xs={12}>
+                        <Card sx={{ maxWidth: 350, boxShadow: '8px 8px 6px #888888', m: '20px', textAlign: 'center' }}>
+                          <CardMedia
+                            component="img"
+                            height="180"
+                            image={`${baseUrl}${classInfo.image}`}
+                            alt="green iguana"
+                          />
+                          <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                              {classInfo.title}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary" style={{ lineHeight: '2',marginBottom:"-25px" }}>
+                              مدرس: &nbsp;
+                              {classInfo.instructor.last_name} {classInfo.instructor.first_name}
+                              <br />
+                              از تاریخ: &nbsp;
+                              {changeDateFormat(classInfo.start_date)}
+                              <br />
+                              تا &nbsp;
+                              {changeDateFormat(classInfo.end_date)}
+                              <br />
+                            </Typography>
+                          </CardContent>
+                          <CardActions>
+                            <Button variant="text" href={classInfo.link} target="_blank" sx={{ margin: '10px auto' }}>
+                              ورود به کلاس
+                            </Button>
+                          </CardActions>
+                        </Card>
+                      </Grid>
+                    )}
+                  </>
+                ))}
+              </Grid>
+            </>
+          )}
         </div>
       </div>
-
       <MyClassesFooter />
     </div>
   );
