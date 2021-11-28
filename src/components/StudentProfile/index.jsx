9f3463dd72429dedf2fb25d3539fb4d1e134fe91
Hanieh Jafari <hani.j.2000@gmail.com>
2021-11-28 14:57:47 +0330

    [feat]: add StudentProfileAboutMe

--- src/components/StudentProfile/index.jsx
+++ src/components/StudentProfile/index.jsx
@@ -2,7 +2,7 @@ import Pro from './Pro';
 import ProBar from './Pro/ProBar';
 import ComeBack from '../../assets/images/StudentProfile/ComeBack.png';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import Avatar from '@mui/material/Avatar';
 import { Button, TextField, Grid, Box, Typography, Container } from '@mui/material';
 import { Link } from 'react-router-dom';
@@ -24,6 +24,218 @@ import { useDispatch, useSelector } from 'react-redux';
 import ReactLoading from 'react-loading';
 import {FormDialog} from './FormDialog';
 import ColorModal from './ColorModal';
+import draftToHtml from 'draftjs-to-html';
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState, ContentState, convertFromHTML , convertToRaw  } from 'draft-js';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+
+function StudentAboutMe(props) {
+  const [validateAfterSubmit, setValidateAfterSubmit] = useState(false);
+  const [apiResponse, setApiResponse] = useState(false);
+  const [values, setValues] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [teacher_data, setteacherData] = useState({});
+  const [editorContent, setEditorContent] = useState(EditorState.createEmpty());
+  let init_content = ""
+  const [editorState, setEditorState] = useState(EditorState.createWithContent(
+    ContentState.createFromBlockArray(
+      convertFromHTML(`<p>درباره من ...</p>`)
+    )));
+  const token = 'JWT ' + localStorage.getItem('access_token');
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios
+        .get(`${baseUrl}/accounts/profile/update-profile/`, {
+          headers: {
+            Authorization: token,
+            'Content-Type': 'application/json',
+          },
+        })
+        .then(response => {
+          console.log(response.data.data.bio);
+          /* setEditorContent(response.data.data.bio); */
+          setEditorState(EditorState.createWithContent(
+            ContentState.createFromBlockArray(
+              convertFromHTML(response.data.data.bio)
+              )
+              ));
+          console.log("content:",editorContent)
+        })
+        .catch(err => {});
+    }
+    fetchData();
+  }, []);
+
+  const cacheRtl = createCache({
+    key: 'muirtl',
+
+    stylisPlugins: [rtlPlugin],
+
+    prepend: true,
+  });
+
+  const editor = useRef(null);
+  const [content, setContent] = useState('');
+  const config = {
+    readonly: false, // all options from https://xdsoft.net/jodit/doc/
+    placeholder: 'درباره من ...',
+    minHeight: 500,
+    editorCssClass: 'about-me',
+    statusbar: false,
+  };
+
+  const onContentStateChange = editorcontent => {
+    setEditorContent(editorcontent);
+  };
+
+  const onEditorStateChange = editorstate => {
+    setEditorState(editorstate);
+  };
+
+
+
+  return (
+    <CacheProvider value={cacheRtl}>
+      <div dir="rtl">
+        <Helmet>
+          <title>پروفایل</title>
+        </Helmet>
+        <ToastContainer />
+
+        <Container maxWidth="lg" component="main" sx={{ margin: 'auto auto 30px auto' }}>
+          <Box
+            sx={{
+              marginTop: 3,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              fontFamily: 'iranyekan',
+            }}
+          >
+            <Typography
+              component="h2"
+              variant="Button"
+              sx={{ color: 'rgba(10, 67, 94, 0.942)', fontSize: { sm: '3vmin', xs: '4vmin' } }}
+            >
+              درباره من
+            </Typography>
+
+            <ToastContainer rtl={true} />
+
+            <Formik
+              enableReinitialize={true}
+              initialValues={{}}
+              onSubmit={async values => {
+                try {
+                  setLoading(true);
+                  console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+                  const body = { bio: draftToHtml(convertToRaw(editorState.getCurrentContent())) };
+                  axios
+                    .put(`${baseUrl}/accounts/profile/update-profile/`, JSON.stringify(body), {
+                      headers: {
+                        Authorization: token,
+                        'Content-Type': 'application/json',
+                      },
+                    })
+                    .then(response => {
+                      console.log('response ', response);
+                      toast.success('با موفقیت به‌روز شد', {
+                        position: 'bottom-center',
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: 'dark',
+                      });
+                      setLoading(false);
+                    })
+                    .catch(err => {
+                      setLoading(false);
+                      console.log('error', err.response.data.message);
+                      toast.error('شرمنده یه بار دیگه امتحان کن', {
+                        position: 'bottom-center',
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: 'dark',
+                      });
+                    });
+                } catch (error) {
+                  console.log('error');
+                }
+              }}
+              validateOnChange={validateAfterSubmit}
+              validate={values => {
+                let error = {};
+
+                return error;
+              }}
+            >
+              {({ handleSubmit, handleChange, setFieldValue, values, errors, handleBlur }) => (
+                <Box
+                  component="form"
+                  id="profile-form"
+                  noValidate
+                  sx={{ mt: 4 }}
+                  onSubmit={e => {
+                    e.preventDefault();
+                    setValidateAfterSubmit(true);
+                    handleSubmit();
+                  }}
+                >
+                  <ToastContainer rtl={true} />
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <div className="wrapper">
+                        <Editor
+                          defaultEditorState={editorState}
+                          editorState={editorState}
+                          editorContent={editorContent}
+                          wrapperClassName="editor-wrapper"
+                          editorClassName="editor-main myColo"
+                          onContentStateChange={onContentStateChange}
+                          onEditorStateChange={onEditorStateChange}
+                          toolbar={{
+                            inline:{inDropdown: true},
+                            list:{inDropdown: true},
+                            textAlign:{inDropdown: true},
+                            link:{inDropdown: true},
+                            history:{inDropdown: true},
+                            
+                          }}
+                        />
+                      </div>
+                    </Grid>
+                  </Grid>
+
+                  <Grid item>
+                    <Button
+                      fullWidth
+                      type="submit"
+                      variant="contained"
+                      sx={{ mt: 3, backgroundColor: `${themeProps.primaryColor} !important`}}
+                      typeof="submit"
+                    >
+                      {!loading && <span>تایید</span>}
+                      {loading && <ReactLoading type="bubbles" color="#fff" className="loading-signup" />}
+                    </Button>
+                  </Grid>
+                </Box>
+              )}
+            </Formik>
+          </Box>
+        </Container>
+      </div>
+    </CacheProvider>
+  );
+}
 
 function SProfile(props) {
   const [validateAfterSubmit, setValidateAfterSubmit] = useState(false);
@@ -310,7 +522,6 @@ function SProfile(props) {
                       />
                     </Grid>
                   </Grid>
-
                   <Grid item>
                     <Button
                       fullWidth
@@ -355,6 +566,8 @@ const StudentProfile = () => {
         >
           <SProfile />
         </div>
+        <div className="Pro__Hello" style={{backgroundColor: themeProps.secondaryColor, boxShadow: `${themeProps.primaryColor} 0px 2px 10px`, color: themeProps.primaryColor }}>
+        <StudentAboutMe/></div>
       </div>
     </div>
   );
