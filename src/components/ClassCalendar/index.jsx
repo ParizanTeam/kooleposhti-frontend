d0e59a8b705dc6293a38076938a1c48fd528993b
Bayan <bayany1379@gmail.com>
2021-12-13 18:00:33 +0330

    [fix]: connect student assignments list to back

--- src/components/ClassCalendar/index.jsx
+++ src/components/ClassCalendar/index.jsx
@@ -13,154 +13,109 @@ import AwakenFox from '../../assets/images/AwakenFox.png';
 import MyClasses from '../MyClasses';
 import MyClassesFooter from '../MyClasses/MyClassesFooter';
 import './style.scss';
-import { Button } from '@mui/material';
+import { Button, useMediaQuery } from '@mui/material';
 import { Link } from 'react-router-dom';
+import axios from '../../utils/axiosConfig';
+import { baseUrl } from '../../utils/constants';
+import ReactLoading from 'react-loading';
 
-function MyAssignments(){
+import ReactHtmlParser from 'react-html-parser';
+import { changeDateFormat2 } from '../../utils/helpers';
+function MyAssignments() {
+  const [loading, setLoading] = React.useState(true);
+
+  const isMobile = useMediaQuery('(max-width: 768px)');
+  const [assignments, setAssignments] = React.useState([]);
+  React.useEffect(() => {
+    axios
+      .get(`${baseUrl}/accounts/students/assignments/`)
+      .then(res => {
+        setAssignments(res.data);
+        console.log('students assignments', res.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.log('error: ', err);
+      });
+  }, []);
+
   const [expanded, setExpanded] = React.useState(false);
 
-  const handleChange = (panel) => (event, isExpanded) => {
+  const handleChange = panel => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
-  //src={expanded?{SleepingFox}:{AwakenFox}}
   return (
-    <div className='AcDiv'>
-      <Accordion expanded={true} onChange={handleChange('panel1')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <img src={AwakenFox} alt='Sleeping Fox'className='FoxP'/>
-          <div className='Text'>
-          <Typography sx={{ width: '33%', flexShrink: 0 ,margin: 1}}>
-            چالش سخنرانی در جمع
-          </Typography>
-          <Typography sx={{ color: 'text.secondary', margin: 1, marginRight:3}}>فردا وقت تحویله!</Typography></div>
-        </AccordionSummary>
-        <AccordionDetails>
-          <div className='rangi'>
-          <Typography>
-            این چالش هفته پیش توسط استاد بخشنده ایجاد شده و فقط یک روز دیگه فرصت داری تا کاملش کنی
-          </Typography></div>
-          <div className='BtRow'>
-          <div className='firstBt'>
-          <Link to='/Student/CourseAssignmentsList'><Button>
-            <p className='Mycolor'>همه تمرینهای این درس</p>
-          </Button></Link></div>
-          <div className='SecBt'><Link to='/Student/CourseAssignments'><Button>
-          <p className='Mycolor'>بریم به صفحه این تمرین</p>
-          </Button></Link></div>
+    <div  className="AcDiv">
+      {loading ? (
+        <div style={{ padding: '140px', display: 'flex',alignItems:'center',justifyContent:'center' }}>
+          <ReactLoading type="spinningBubbles" color="white" height={100} width={100} />
         </div>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2bh-content"
-          id="panel2bh-header"
-        >
-          <img src={(expanded === 'panel2')?AwakenFox:SleepingFox} alt='AwakenFox'className='FoxP'/>
-          <div className='Text'>
-          <Typography sx={{ width: '33%', flexShrink: 0,margin: 1 }}>چالش نقاشی دایناسور</Typography>
-          <Typography sx={{ color: 'text.secondary',margin: 1, marginRight:3 }}>
-            تا شنبه وقت داری!
-          </Typography></div>
-        </AccordionSummary>
-        <AccordionDetails>
-        <div className='rangi'>
-          <Typography>
-          این چالش هفته پیش توسط استاد بخشنده ایجاد شده و دو روز دیگه فرصت داری تا کاملش کنی
-          </Typography></div>
-          <div className='BtRow'>
-          <div className='firstBt'>
-          <Link to='*'><Button>
-            <p className='Mycolor'>همه تمرینهای این درس</p>
-          </Button></Link></div>
-          <div className='SecBt'><Link to='*'><Button>
-          <p className='Mycolor'>بریم به صفحه این تمرین</p>
-          </Button></Link></div>
+      ) : (
+        <div>
+          {assignments.map((assignment, idx) => (
+            <Accordion expanded={idx == 0 ? true : expanded === idx} onChange={handleChange(idx)}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon className="assignment-margin" />}>
+                <img
+                  src={idx == 0 || expanded === idx ? AwakenFox : SleepingFox}
+                  alt="Sleeping Fox"
+                  className="FoxP assignment-margin"
+                />
+                <div className="Text">
+                  <Typography sx={{ width: '33%', flexShrink: 0, margin: 1 }}>{assignment.title}</Typography>
+                  <Typography sx={{ color: 'text.secondary', margin: 1, marginRight: 3 }}>
+                    تا&nbsp;
+                    {changeDateFormat2(assignment.end_date)} &nbsp; وقت داری
+                  </Typography>
+
+                  {/* {!isMobile && (
+                    <Typography sx={{ width: '33%', flexShrink: 0, margin: 1 }}>درس: آموزش رنگ ها</Typography>
+                  )} */}
+                </div>
+              </AccordionSummary>
+              <AccordionDetails>
+                <div className="rangi assignment-margin">
+                  {/* {isMobile && (
+                    <Typography align="right" sx={{ marginBottom: '10px' }}>
+                      درس: آموزش رنگ ها
+                    </Typography>
+                  )} */}
+                  <Typography variant="h6" align="right" sx={{ marginBottom: '10px', color: '#c83636' }}>
+                    صورت سوال:
+                  </Typography>
+                  <div className="assignment-question">{ReactHtmlParser(assignment.question)}</div>
+                </div>
+                <div className="BtRow">
+                  <div className="firstBt">
+                    <Button component={Link} to={`/dashboard/class/${assignment.course}/assignments`}>
+                      <p className="Mycolor">همه تمرینهای این درس</p>
+                    </Button>
+                  </div>
+                  <div className="SecBt">
+                    <Button component={Link} to={`/dashboard/class/${assignment.course}/assignments/${assignment.id}`}>
+                      <p className="Mycolor">بریم به صفحه این تمرین</p>
+                    </Button>
+                  </div>
+                </div>
+              </AccordionDetails>
+            </Accordion>
+          ))}
         </div>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3bh-content"
-          id="panel3bh-header"
-        >
-          <img src={(expanded === 'panel3')?AwakenFox:SleepingFox} alt='PlayfulFox'className='FoxP'/>
-          <div className='Text'>
-          <Typography sx={{ width: '33%', flexShrink: 0,margin: 1 }}>
-            چالش شطرنج
-          </Typography>
-          <Typography sx={{ color: 'text.secondary' ,margin: 1, marginRight:3}}>
-            دوشنبه آخرین فرصته...
-          </Typography></div>
-        </AccordionSummary>
-        <AccordionDetails>
-        <div className='rangi'>
-          <Typography> 
-          این چالش سه روز پیش توسط استاد بخشنده ایجاد شده و چهار روز دیگه فرصت داری تا کاملش کنی
-          </Typography></div>
-          <div className='BtRow'>
-          <div className='firstBt'>
-          <Link to='*'><Button>
-            <p className='Mycolor'>همه تمرینهای این درس</p>
-          </Button></Link></div>
-          <div className='SecBt'><Link to='*'><Button>
-          <p className='Mycolor'>بریم به صفحه این تمرین</p>
-          </Button></Link></div>
-        </div>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')} > 
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel4bh-content"
-          id="panel4bh-header"
-        >
-          <img src={(expanded === 'panel4')?AwakenFox:SleepingFox} alt='HappyFox'className='FoxP'/>
-          <div className='Text'>
-          <Typography sx={{ width: '33%', flexShrink: 0 ,margin: 1}}>چالش نویسندگی خلاقانه</Typography>
-          <Typography sx={{ color: 'text.secondary',margin: 1 , marginRight:3}}>
-            تا پنجشنبه فقط وقت داری
-          </Typography>
-          </div>
-        </AccordionSummary>
-        <AccordionDetails>
-        <div className='rangi'>
-          <Typography>
-          این چالش دیروز توسط استاد بخشنده ایجاد شده و یک هفته دیگه فرصت داری تا کاملش کنی
-          </Typography>
-          </div>
-          <div className='BtRow'>
-          <div className='firstBt'>
-          <Link to='*'><Button>
-            <p className='Mycolor'>همه تمرینهای این درس</p>
-          </Button></Link></div>
-          <div className='SecBt'><Link to='*'><Button>
-          <p className='Mycolor'>بریم به صفحه این تمرین</p>
-          </Button></Link></div>
-        </div>
-        </AccordionDetails>
-      </Accordion>
+      )}
     </div>
   );
 }
 
-
 const ClassCalendar = () => {
   return (
-  <div>
-    <MyClasses/>
-    <img src='https://8pic.ir/uploads/1307925801537355428-128.png' alt='cc' className='ccImg'/>
-    <br/>
-    <div className="afterMyC-b">
-      <MyAssignments />
+    <div>
+      <MyClasses />
+      <img src="https://8pic.ir/uploads/1307925801537355428-128.png" alt="cc" className="ccImg" />
+      <br />
+      <div className="afterMyC-b">
+        <MyAssignments />
+      </div>
+      <MyClassesFooter />
     </div>
-    <MyClassesFooter />
-  </div>
   );
 };
 export default ClassCalendar;
