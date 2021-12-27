1007697d6e19175a4dedea1d0942572163db5568
Emadmousavi <emadmoosavi79@gmail.com>
2021-12-28 01:23:32 +0330

    [chore] minor changes in home page

--- src/components/BestTeachers/index.jsx
+++ src/components/BestTeachers/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Tooltip } from '@mui/material';
+import { Tooltip, Grid } from '@mui/material';
 import { bestTeachersData } from './bestTeachersData';
 import 'swiper/swiper-bundle.min.css';
 import './style.scss';
@@ -11,20 +11,24 @@ const BestTeachers = () => {
       <h2 className="best-teachers__title">
         از <span className="best-teachers__highlight">بهترین اساتید</span> یاد بگیر!
       </h2>
-      <Swiper spaceBetween={10} slidesPerView={'auto'}>
-        {bestTeachersData.map(BestTeacher => (
-          <SwiperSlide key={BestTeacher.id}>
-            <a href="#">
-              <Tooltip
-                title={<span style={{ fontFamily: 'iranyekan' }}>{BestTeacher.teacherName}</span>}
-                placement="bottom"
-              >
-                <img className="best-teachers__teacher-img" src={BestTeacher.teacherImgSrc} />
-              </Tooltip>
-            </a>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <Grid container sx={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <Grid item>
+          <Swiper spaceBetween={10} slidesPerView={'auto'}>
+            {bestTeachersData.map(BestTeacher => (
+              <SwiperSlide key={BestTeacher.id}>
+                <a href="#">
+                  <Tooltip
+                    title={<span style={{ fontFamily: 'iranyekan' }}>{BestTeacher.teacherName}</span>}
+                    placement="bottom"
+                  >
+                    <img className="best-teachers__teacher-img" src={BestTeacher.teacherImgSrc} />
+                  </Tooltip>
+                </a>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
