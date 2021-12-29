dafe39fb0a55248278612fffb9bfced879fb8bdb
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2021-12-29 20:35:56 +0330

    [fix]: comment null first and last name

--- src/components/CourseComments/CourseCommentStudent/index.jsx
+++ src/components/CourseComments/CourseCommentStudent/index.jsx
@@ -10,25 +10,27 @@ function CourseCommentStudent(props) {
     <React.Fragment>
       <Grid container>
         <Grid item sm={1} xs={0} sx={{ display: { sm: 'flex', xs: 'none' }, flexDirection: 'column' }} md={1}>
-          <Avatar
-            className="comment-avatar"
-            src={props.info.image ? props.info.image : avatar}
-            alt="avatar"
-          />
+          <Avatar className="comment-avatar" src={props.info.image ? props.info.image : avatar} alt="avatar" />
         </Grid>
         <Grid item sm={11} xs={12}>
           <div className="course-comment-student__card">
             <Grid container>
               <Grid item xs={12}>
                 <Typography variant="body4" className="course-comment-student__name">
-                  {`${props.info.first_name} ${props.info.last_name}`}
+                  {`${
+                    props.info.first_name && props.info.last_name
+                      ? props.info.first_name + props.info.last_name
+                      : props.info.username
+                  }`}
                 </Typography>
               </Grid>
               <Grid item xs={12} mt={1}>
                 <Typography className="course-comment-student__date">
-                {`${convertNumberToPersian(date.getHours())}:${convertNumberToPersian(date.getMinutes())}`}
+                  {`${convertNumberToPersian(date.getHours())}:${convertNumberToPersian(date.getMinutes())}`}
                   <span> </span>
-                  {`${convertNumberToPersian(date.getFullYear())}/${convertNumberToPersian(date.getMonth())}/${convertNumberToPersian(date.getDate())}`}
+                  {`${convertNumberToPersian(date.getFullYear())}/${convertNumberToPersian(
+                    date.getMonth()
+                  )}/${convertNumberToPersian(date.getDate())}`}
                 </Typography>
               </Grid>
               <Grid item xs={12} mt={3} className="course-comment-student__text">
