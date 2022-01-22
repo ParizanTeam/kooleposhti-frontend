9b9801f1b95f1159335c0bc5ec85d0c173522232
Bayan <bayany1379@gmail.com>
2022-01-22 20:14:18 +0330

    [fix]: bookmarked classes slider loop

--- src/components/StudentDashboardBookMarkedClasses/index.jsx
+++ src/components/StudentDashboardBookMarkedClasses/index.jsx
@@ -13,7 +13,7 @@ import { useSelector } from 'react-redux';
 //import { useMobile } from '../../utils/detectSource';
 import * as React from 'react';
 import './style.scss';
-import {useMediaQuery } from '@mui/material';
+import { useMediaQuery } from '@mui/material';
 import axios from '../../utils/axiosConfig';
 import { baseUrl } from '../../utils/constants';
 import { change_profile_color } from '../../store/actions';
@@ -22,7 +22,7 @@ import noPersonImg from '../../assets/images/noperson.png';
 SwiperCore.use([Navigation, Keyboard]);
 const MyCourseSlider = () => {
   const themeProps = useSelector(state => state.theme);
-  let theNewone = localStorage.getItem("chosenColor");
+  let theNewone = localStorage.getItem('chosenColor');
   change_profile_color(theNewone);
   const [loading, setLoading] = React.useState(true);
   const isMobile = useMediaQuery('(max-width: 768px)');
@@ -30,16 +30,17 @@ const MyCourseSlider = () => {
   React.useEffect(() => {
     async function fetchData() {
       await axios
-      .get(`${baseUrl}/accounts/students/favorites/`)
-      .then(res => {
-        setfavorites(res.data);
-        console.log('favorite classes', res.data);
-        setLoading(false);
-      })
-      .catch(err => {
-        console.log('error: ', err);
-      });}
-      fetchData();
+        .get(`${baseUrl}/accounts/students/favorites/`)
+        .then(res => {
+          setfavorites(res.data);
+          console.log('favorite classes', res.data);
+          setLoading(false);
+        })
+        .catch(err => {
+          console.log('error: ', err);
+        });
+    }
+    fetchData();
   }, []);
   return (
     <div className="My-courses-section">
@@ -47,37 +48,39 @@ const MyCourseSlider = () => {
         مورد <span className="My-courses-section__highlight"> &#x2665; </span>ها
       </h2>
       <div className="My-carousal-container">
-      {loading || favorites.length <= 0? (
-              <div style={{ padding: '100px', display: 'flex' }}>
-                <ReactLoading type="spinningBubbles" color={themeProps.primaryColor} height={100} width={100} />{' '}
-              </div>
-            ) : (
-        <div style={{ width: isMobile ? '100%' : '90%' }}>
-          <Swiper
-            spaceBetween={10}
-            slidesPerView={'auto'}
-            centeredSlides
-            navigation={isMobile? false : true}
-            loop
-            keyboard
-            centeredSlides
-          >
-            {favorites.map(item => (
-              <SwiperSlide key={item.id}>
-                <CourseCard
-                  title={item.title}
-                  teacherName={item.instructor.last_name}
-                  rate={item.rate}
-                  teacherImgSrc={(item.instructor.image && baseUrl + item.instructor.image) || noPersonImg}
-                  imgSrc={
-                    (item.image && baseUrl + item.image) ||
-                    'https://www.inklyo.com/wp-content/uploads/How-to-Succeed-in-an-Online-Course.jpg'
-                  }
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>)}
+        {loading || favorites.length <= 0 ? (
+          <div style={{ padding: '100px', display: 'flex' }}>
+            <ReactLoading type="spinningBubbles" color={themeProps.primaryColor} height={100} width={100} />{' '}
+          </div>
+        ) : (
+          <div style={{ width: isMobile ? '100%' : '90%' }}>
+            <Swiper
+              spaceBetween={10}
+              slidesPerView={'auto'}
+              centeredSlides
+              navigation={isMobile || favorites.length <= 1 ? false : true}
+              keyboard
+            >
+              {favorites.map(item => (
+                <SwiperSlide key={item.id}>
+                  <CourseCard
+                    id={item.id}
+                    username={item.instructor.username}
+                    title={item.title}
+                    teacherName={item.instructor.first_name + ' ' + item.instructor.last_name}
+                    rate={item.rate}
+                    teacherImgSrc={(item.instructor.image && baseUrl + item.instructor.image) || noPersonImg}
+                    imgSrc={
+                      (item.image && baseUrl + item.image) ||
+                      'https://www.inklyo.com/wp-content/uploads/How-to-Succeed-in-an-Online-Course.jpg'
+                    }
+                    isCFavorite={item.is_favorite}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -85,18 +88,19 @@ const MyCourseSlider = () => {
 
 const StudentDashboardBookMarkedClasses = () => {
   const themeProps = useSelector(state => state.theme);
-  let theNewone = localStorage.getItem("chosenColor");
+  let theNewone = localStorage.getItem('chosenColor');
   change_profile_color(theNewone);
   return (
-  <div>
-    <StudentDashboardHeader />
-    <img src={themeProps.btnLabel} alt='bm' className='bmImg'/>
-    <br/><br/>
-    <div className="afterMyC-a">
-      <MyCourseSlider/>
+    <div>
+      <StudentDashboardHeader />
+      <img src={themeProps.btnLabel} alt="bm" className="bmImg" />
+      <br />
+      <br />
+      <div className="afterMyC-a">
+        <MyCourseSlider />
+      </div>
+      <StudentDashboardFooter />
     </div>
-    <StudentDashboardFooter />
-  </div>
   );
 };
 export default StudentDashboardBookMarkedClasses;
